f9214250878e716954b9174f502f4a5ea4d6805a
buzarini <43939914+buzarini@users.noreply.github.com>
2025-03-27 06:14:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { getWarpConfigLink } = require('./AWGp');
-const { getWarpConfigLink2 } = require('./Karing');
-const { getWarpConfigLink3 } = require('./WarpInWarp');
-const { getWarpConfigLink4 } = require('./AWGr');
-const { getWarpConfigLink5 } = require('./Neko');
-const { getWarpConfigLink6 } = require('./AWGm');
-const { getWarpConfigLink7 } = require('./Clash');
-const { getWarpConfigLink8 } = require('./Husi');
+const { getWarpConfigLink } = require('./AWGrm');
+const { getWarpConfigLink2 } = require('./AWGrm2');
+const { getWarpConfigLink3 } = require('./AWGrm3');
+const { getWarpConfigLink4 } = require('./AWGrm4');
+const { getWarpConfigLink5 } = require('./AWGrm5');
+const { getWarpConfigLink6 } = require('./AWGrm6');
+const { getWarpConfigLink7 } = require('./AWGrp');
+const { getWarpConfigLink8 } = require('./AWGrp2');
+const { getWarpConfigLink9 } = require('./AWGrp3');
+const { getWarpConfigLink10 } = require('./AWGrp4');
+const { getWarpConfigLink11 } = require('./AWGrp5');
+const { getWarpConfigLink12 } = require('./AWGrp6');
 const path = require('path');
 
 const app = express();
@@ -121,6 +125,62 @@ app.get('/warp7', async (req, res) => {
 app.get('/warp8', async (req, res) => {
     try {
         const content = await getWarpConfigLink8();
+        if (content) {
+            res.json({ success: true, content });
+        } else {
+            res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке запроса:', error);
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+    }
+});
+
+app.get('/warp9', async (req, res) => {
+    try {
+        const content = await getWarpConfigLink9();
+        if (content) {
+            res.json({ success: true, content });
+        } else {
+            res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке запроса:', error);
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+    }
+});
+
+app.get('/warp10', async (req, res) => {
+    try {
+        const content = await getWarpConfigLink10();
+        if (content) {
+            res.json({ success: true, content });
+        } else {
+            res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке запроса:', error);
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+    }
+});
+
+app.get('/warp11', async (req, res) => {
+    try {
+        const content = await getWarpConfigLink11();
+        if (content) {
+            res.json({ success: true, content });
+        } else {
+            res.status(500).json({ success: false, message: 'Не удалось сгенерировать конфиг.' });
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке запроса:', error);
+        res.status(500).json({ success: false, message: 'Произошла ошибка на сервере.' });
+    }
+});
+
+app.get('/warp12', async (req, res) => {
+    try {
+        const content = await getWarpConfigLink12();
         if (content) {
             res.json({ success: true, content });
         } else {
